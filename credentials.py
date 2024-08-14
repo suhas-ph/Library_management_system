25597c3777e64b8f38aa27b20d34f029e614321f
suhas-ph <127218737+suhas-ph@users.noreply.github.com>
2024-08-14 21:59:14 +0530

    Add files via upload

---
+++ credentials.py
@@ -0,0 +1,5 @@
+# User Credentials
+host = 'localhost'
+user = 'root'
+#password = remove the hash and can add ur password'
+database = 'library_management'
---
+++ customs.py
@@ -0,0 +1,15 @@
+color_1 = "#262A76"
+color_2 = "#B6B0FF"
+color_3 = "black"
+color_4 = "white"
+color_5 = "red"
+color_6 = "#8250C4"
+color_7 = "#8250C4"
+color_10="#8250C4"
+font_1 = "times new roman"
+font_2 = "helvetica"
+
+columns = ('book_id', 'book_name', 'author', 'edition', 'price', 'qty')
+
+columns_1 = ('book_id', 'book_name', 'student_roll', 'student_name', 'course', 'subject', 'issue_date', 'return_date')
+ 
